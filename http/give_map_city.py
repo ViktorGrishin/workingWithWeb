029dd047b497f62d8dfb2695795f2fa04859b042
ViktorGrishin <gishin06Vt@yandex.ru>
2024-03-12 17:39:07 +0700

    Создал программу, выдающей снимок карты города из константы

---
+++ http/give_map_city.py
@@ -0,0 +1,50 @@
+import os
+import sys
+
+import pygame
+import requests
+
+CITY = "Йошкар-Ола"
+SIZE = 10
+
+geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={CITY}&format=json"
+response = requests.get(geocoder_request)
+if response:
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+    # Координаты центра топонима:
+    toponym_coodrinates = tuple(map(float, toponym["Point"]["pos"].split()))
+else:
+    print("Ошибка выполнения запроса:")
+    print(geocoder_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(response.status_code)
+
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={toponym_coodrinates[0]},{toponym_coodrinates[1]}&z={SIZE}&l=map"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(response.status_code)
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+# Удаляем за собой файл с изображением.
+os.remove(map_file)
